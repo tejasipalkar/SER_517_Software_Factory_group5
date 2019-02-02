71a0ba27941e0e06f1d4b056f90c85fec7bfcb57
kbhangal <kbhangal@asu.edu>
2019-02-02 13:25:37 -0700

    add login form

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'fe36a4a2322786622f1ef490c59b8b2f'
 
 @app.route("/")
 @app.route("/home")
@@ -10,6 +13,17 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'kbhangal@asu.edu' and form.username.data == 'kbhangal':   #TODO: use DB
+            flash(format('Login Successful. Welcome!'), 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(format('Login Unsuccessful. Please check username and password!'), 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
