9d4834532ae8b80d87a775c797e9f5e82b695e12
tpalkar92@gmail.com <tpalkar92@gmail.com>
2019-02-03 17:26:34 -0700

    Updated application.py to add the content to Home page

--- application.py
+++ application.py
@@ -1,15 +1,32 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+courses =[{
+    'name':'SER 517',
+    'Instructor' :'Heinrich Reimer'
+    },
+    {
+        'name' : 'SER 515',
+        'Instructor' :'Alexandra Mehlhase'
+    
+    },
+    {
+        'name' : 'SER 422',
+        'Instructor' :'Kevin Gary'
+    
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html',title ="Home",courses= courses)
+
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title ="About")
 
 if __name__ == '__main__':
     app.run(debug=True)
