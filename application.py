f8cf7dbfc660a41372ee3613e92bd83420148430
tpalkar92@gmail.com <tpalkar92@gmail.com>
2019-02-04 15:25:19 -0700

    Linked forms on Home page to course page

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for,json
+from flask import Flask, render_template, url_for,json, request
 app = Flask(__name__)
 
 with open('courseslist.json') as f:
@@ -9,11 +9,18 @@ with open('courseslist.json') as f:
 def home():
     return render_template('home.html',title ="Home",courses= courses)
 
-
-
 @app.route("/about")
 def about():
     return render_template('about.html',title ="About")
+
+@app.route("/")
+
+@app.route('/send', methods=['GET','POST'])
+def send():
+    if request.method== 'POST':
+        return render_template('coursepage.html',title = "Course Page")
+
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
