32e02f62c7d839ee79603a4d672f9f1d4e4524ee
kbhangal <kbhangal@asu.edu>
2019-02-07 22:56:09 -0700

    update login authentication

--- instructorTool/__init__.py
+++ instructorTool/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fe36a4a2322786622f1ef490c59b8b2f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instructor.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from instructorTool import routes
--- instructorTool/routes.py
+++ instructorTool/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from instructorTool import app
 from instructorTool.forms import LoginForm
+from instructorTool.models import User
+from flask_login import login_user, current_user, logout_user, login_required
 
+#if logged-in only then take user to home page and show list of courses
+# else redirect user to login page and show alert that they need to log in first
 @app.route("/")
 @app.route("/home")
 def home():
@@ -11,13 +15,32 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route("/course")
+def course():
+    return render_template('course.html')
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'kbhangal@asu.edu' and form.username.data == 'kbhangal':   #TODO: use DB
-            flash(format('Login Successful. Welcome!'), 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and (user.password == form.password.data):
+                login_user(user, remember=form.remember.data)
+                flash(format('Login Successful. Welcome!'), 'success')
+                next_page = request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash(format('Login Unsuccessful. Please check username and password!'), 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html')
