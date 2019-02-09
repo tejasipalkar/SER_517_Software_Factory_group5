558301e34d9f125d55cda13efe50e71a42fcd3db
Zain Siddiqui <32204330+zain2511@users.noreply.github.com>
2019-02-09 00:20:52 -0700

    Partial code for grouping algorithm

---
+++ ResearchCode/grp.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import csv
+import numpy
+
+
+def main():
+
+	print("Enter number of students in a Team:", end="")
+	no_of_stu = int(input())
+
+	stu_available = set()
+	stu_list = []
+	stu_pref = {}
+	stu_avoid = {}
+
+	with open("StudentSurvey.csv") as fp:
+		reader = csv.reader(fp)
+		count = 0
+		for line in reader:
+			#print(line)
+			if count > 0:
+				stu_available.add(line[1])
+				stu_list.append(line[1])
+				stu_pref[line[1]] = line[2].split(', ')
+				stu_avoid[line[1]] = line[3].split(', ')
+
+			count += 1
+	n = len(stu_available)
+	
+	team_map = {x:0 for x in range(1, (n // no_of_stu) + 1)}
+	stu_dist = numpy.zeros(shape=(n,n))
+	#print(stu_list)
+	#print("map1", stu_pref)
+	
+	#create distance matrix
+	for i in range(n):
+		# for j in range(n):
+		# 	if i == j:
+		# 		stu_dist[i][j] += n
+		# 	else:
+		temp1 = stu_pref[stu_list[i]]
+		if temp1[0] != '' and temp1[0] != ' ':
+			a = 0
+			while a < len(temp1):
+				#print(temp1)
+				idx = stu_list.index(temp1[a])
+				stu_dist[i][idx] -= n
+				if stu_list[i] in stu_pref[stu_list[idx]]:
+					stu_dist[i][idx] -= n
+				a += 1
+
+		temp2 = stu_avoid[stu_list[i]]
+		if temp2[0] != '' and temp2[0] != ' ':
+
+			b = 0
+			while b < len(temp2):
+				#print(temp2[b], temp2)
+				idx = stu_list.index(temp2[b])
+				stu_dist[i][idx] += n
+				if stu_list[i] in stu_avoid[stu_list[idx]]:
+					stu_dist[i][idx] += n
+				b += 1
+				
+
+	print(stu_dist)
+	print(team_map)
+
+	#Assign Preference to the team
+	
+	team_list = []
+	for i,s in enumerate(stu_dist):
+		print(s)
+		#for i in range(n):
+		if stu_list[i] in stu_available:
+			temp_team = []
+			count_member = 1
+			temp_team.append(stu_list[i])
+			stu_available.remove(stu_list[i])
+			while no_of_stu > len(temp_team) and min(s) < 0:
+				idx_min = numpy.argmin(s)
+				if stu_list[idx_min] in stu_available:
+					temp_team.append(stu_list[idx_min])
+					stu_available.remove(stu_list[idx_min])
+					
+				s[idx_min] = 0
+
+				count_member += 1
+
+
+			print("updated", s)		
+			print(temp_team)
+			team_list.append(temp_team)
+
+	# Sort the list in descending order of len
+	#print(team_list)
+	#sorted(team_list, key = lambda x:len(x) )
+	team_list.sort(key=len, reverse=True)
+	#print(team_list)
+	remaining_stu = [item for sublist in team_list[len(team_map):] for item in sublist]
+	team_list = team_list[:len(team_map)]
+	print(team_list)
+	print(remaining_stu)
+	
+
+
+
+
+if __name__ == "__main__":
+	main()
+	
+	
