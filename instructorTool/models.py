1801071ed11672f80c622c788512acd883642b69
Ganesh Kumar <svganesh93@gmail.com>
2019-02-12 18:25:57 -0700

    redefining models - removing password

--- instructorTool/models.py
+++ instructorTool/models.py
@@ -2,14 +2,17 @@ from instructorTool import db, login_manager
 from flask_login import UserMixin
 
 @login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+def load_user(id):
+	return User.query.get(int(id))
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), nullable=False)
+	email = db.Column(db.String(20), unique=True, nullable=False)
 
-    def __repr__(self):
-        return "User('{}',{},{})".format({self.username},{self.email},{self.password})
+	def __init__(self, username, email):
+		self.username = username
+		self.email = email
+
+	def __repr__(self):
+		return "User('{}',{})".format({self.username},{self.email})
