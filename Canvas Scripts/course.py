62ef0af241e89772de94cbdc1fcd3001d658ed42
Yathartha Goel <ygoel@asu.edu>
2019-02-16 12:33:52 -0700

    Can retrieve instructor name from a course

--- Canvas Scripts/course.py
+++ Canvas Scripts/course.py
@@ -4,11 +4,19 @@ API_KEY = "7236~o5XXfM7GrZZogzsg8xdQoODn3DdBqdwlq2DOM9qo4uD7q3e1Y79Ssi9vmObH9q42
 canvas = Canvas(API_URL, API_KEY)
 
 courses = canvas.get_courses()
-# counter = 0
-# for vals in accounts:
-#     if counter == 3:
-#         term = vals.get_users()
-#     counter = counter + 1
+counter = 0
+for vals in courses:
+    if counter == 3:
+        term = vals.get_enrollments()
+        users = vals.get_users()
+    counter = counter + 1
 
-# for vals in term:
-#     print(vals)
+for vals in term:
+    if vals.type == "TeacherEnrollment":
+        user_id = vals.user_id
+
+for vals in users:
+   if vals.id == user_id:
+        instructor_name = vals.name
+
+print(instructor_name)
