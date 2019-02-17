4bad93fb060894bfa7444f1a3637c173bd992da2
Yathartha Goel <ygoel@asu.edu>
2019-02-17 10:46:14 -0700

    Made more object oriented and improved naming conventions

--- Canvas Scripts/course.py
+++ Canvas Scripts/course.py
@@ -1,25 +1,28 @@
 from canvasapi import Canvas
-API_URL = "https://asu.instructure.com"
-API_KEY = "7236~o5XXfM7GrZZogzsg8xdQoODn3DdBqdwlq2DOM9qo4uD7q3e1Y79Ssi9vmObH9q42"
-canvas = Canvas(API_URL, API_KEY)
 
-def getcourse():
-    courses = canvas.get_courses()
-    course_dict = dict()
-    for vals in courses:
-        course_name = vals.name
-        enrollments = vals.get_enrollments()
-        for items in enrollments:
-            if items.type == "TeacherEnrollment":
-                user_id = items.user_id
-        users = vals.get_users()
-        for items in users:
-            if items.id == user_id:
-                instructor_name = items.name
+class Course:
+    def __init__(self):
+        self.API_URL = "https://asu.instructure.com"
+        self.API_KEY = "7236~o5XXfM7GrZZogzsg8xdQoODn3DdBqdwlq2DOM9qo4uD7q3e1Y79Ssi9vmObH9q42"
+        self.canvas = Canvas(self.API_URL, self.API_KEY)
 
-        if course_name not in course_dict:
-            course_dict[course_name] = instructor_name
+    def getcourse(self):
+        courses = self.canvas.get_courses()
+        course_dict = dict()
+        for course in courses:
+            course_name = course.name
+            enrollments = course.get_enrollments()
+            for enrollment in enrollments:
+                if enrollment.type == "TeacherEnrollment":
+                    user_id = enrollment.user_id
+            users = course.get_users()
+            for user in users:
+                if user.id == user_id:
+                    instructor_name = user.name
 
-    return course_dict
+            if course_name not in course_dict:
+                course_dict[course_name] = instructor_name
+
+        return course_dict
 
 
