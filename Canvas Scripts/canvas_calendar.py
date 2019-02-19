eb682de6b2625d10f1cd1f18958634a5ba75b7c2
Yathartha Goel <ygoel@asu.edu>
2019-02-18 21:09:42 -0700

    Now shows all the events including assignments and stores them into json

--- Canvas Scripts/canvas_calendar.py
+++ Canvas Scripts/canvas_calendar.py
@@ -1,5 +1,5 @@
 from canvasapi import Canvas
-# from canvasapi.calendar_event import CalendarEvent
+import json
 API_URL = "https://asu.instructure.com"
 API_KEY = "7236~o5XXfM7GrZZogzsg8xdQoODn3DdBqdwlq2DOM9qo4uD7q3e1Y79Ssi9vmObH9q42"
 canvas = Canvas(API_URL, API_KEY)
@@ -8,11 +8,21 @@ def getallevents():
     courses = canvas.get_courses()
     user = canvas.get_user('self')
     courses_array = []
+    user_array = []
+    resultant_json_format = {"events":[], "assignments":[]}
     for course in courses:
         courses_array.append('course_' + str(course.id))
-    courses_array.append('user_' + str(user.id))
-    events = canvas.get_calendar_events(all_events = 1, context_codes=courses_array)
-    for event in events:
-        print(event)
+    user_array.append('user_' + str(user.id))
+    course_events = canvas.get_calendar_events(all_events = 1, type = 'event', context_codes=courses_array)
+    user_events = canvas.get_calendar_events(all_events = 1, type = 'event', context_codes=user_array)
+    course_assignments = canvas.get_calendar_events(all_events = 1, type = 'assignment', context_codes=courses_array)
+    for event in course_events:
+        resultant_json_format["events"].append(event.__dict__['attributes'])
+    for event in user_events:
+        resultant_json_format["events"].append(event.__dict__['attributes'])
+    for assignment in course_assignments:
+        resultant_json_format["assignments"].append(assignment.__dict__['assignment'])
 
-getallevents()
+    with open('calendar.json', 'a') as fp:
+            json.dump(resultant_json_format, fp, indent = 4)
+
