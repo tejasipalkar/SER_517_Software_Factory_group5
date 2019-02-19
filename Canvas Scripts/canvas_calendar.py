4bddcb7bec82b6e522eb1b433023c15b59c69052
Yathartha Goel <ygoel@asu.edu>
2019-02-18 20:03:04 -0700

    Made changes to get all the calendar events objects

--- Canvas Scripts/canvas_calendar.py
+++ Canvas Scripts/canvas_calendar.py
@@ -4,9 +4,15 @@ API_URL = "https://asu.instructure.com"
 API_KEY = "7236~o5XXfM7GrZZogzsg8xdQoODn3DdBqdwlq2DOM9qo4uD7q3e1Y79Ssi9vmObH9q42"
 canvas = Canvas(API_URL, API_KEY)
 
-def getevents():
-    events = canvas.get_calendar_events(all_events = 1, context_codes=['course_15760'])
+def getallevents():
+    courses = canvas.get_courses()
+    user = canvas.get_user('self')
+    courses_array = []
+    for course in courses:
+        courses_array.append('course_' + str(course.id))
+    courses_array.append('user_' + str(user.id))
+    events = canvas.get_calendar_events(all_events = 1, context_codes=courses_array)
     for event in events:
         print(event)
 
-getevents()
+getallevents()
