93fafa317d9148a805868f001972cc6507eda0ca
kbhangal <kbhangal@asu.edu>
2019-03-01 17:29:44 -0700

    add drag and drop example event

--- instructorTool/static/javascript/calendar/calendar.js
+++ instructorTool/static/javascript/calendar/calendar.js
@@ -6,6 +6,20 @@ const DEFAULT_EVENT_COLOR = ""
 
 $(document).ready(function() {
 
+  $('#external-events .fc-event').each(function() {
+    // store data so the calendar knows to render an event upon drop
+    $(this).data('event', {
+      title: $.trim($(this).text()), // use the element's text as the event title
+      stick: true // maintain when user navigates (see docs on the renderEvent method)
+    });
+    // make the event draggable using jQuery UI
+    $(this).draggable({
+      zIndex: 999,
+      revert: true,      // will cause the event to go back to its
+      revertDuration: 0  //  original position after the drag
+    });
+  });
+
   $('#calendar').fullCalendar({
     header: {
       left: 'prev,next today',
@@ -17,6 +31,8 @@ $(document).ready(function() {
     eventLimit: true, // allow "more" link when too many events
     selectable: true,
     selectHelper: true,
+    droppable: true,
+    eventTextColor: '#ffffff',
     timezone: "local",
     select: function(start, end) {
       console.log("Select date",end)
