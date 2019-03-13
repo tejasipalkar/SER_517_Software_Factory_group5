4d1c55c9e4897b25e0d578b2d04468daa91ddde0
Zain Siddiqui <32204330+zain2511@users.noreply.github.com>
2019-03-12 20:24:47 -0700

    Made changes as per client requirements.

--- ResearchCode/scrape.py
+++ ResearchCode/scrape.py
@@ -8,7 +8,7 @@ def main():
 	print("How many student avoidance is allowed?", end='')
 	no_of_avoid = int(input())
 
-	with open("Scrape.csv") as fp:
+	with open("scrape.csv") as fp:
 		reader = csv.reader(fp, delimiter=',')
 		count = 1
 		res = []
@@ -17,19 +17,26 @@ def main():
 			i = 0
 			if count > 3:
 				#print(line)
-				temp.append(line[i] + '@asu.edu')
+				temp.append(line[i])
+				i+=1
+				temp.append(line[i])
+				i+=1
+				temp.append(line[i])
+				i+=1
+				temp.append(line[i])
 				i += 1
 				pref = []
-				for k in range(i, no_of_pref):
+				for k in range(i, no_of_pref+i):
+					#print(line[k])
 					if line[k] != '' and line[k] != ' ':
 						#print(line[k])
-						pref.append(line[k] + '@asu.edu')
+						pref.append(line[k])
 				temp.append(pref)
 				i += no_of_pref
 				avoid = []
-				for k in range(i, no_of_avoid):
-					if line[k] != '' or line[k] != ' ':
-						avoid.append(line[k] + '@asu.edu')
+				for k in range(i, no_of_avoid+i):
+					if line[k] != '' and line[k] != ' ':
+						avoid.append(line[k])
 				temp.append(avoid)
 				i += no_of_avoid
 				temp.append(line[i])
@@ -41,7 +48,16 @@ def main():
 					else:
 						date_time.append(first + '-' + word)
 
+
 				temp.append(date_time)
+				i += 1
+				temp.append(line[i])
+				i += 1
+				temp.append(line[i])
+				i += 1
+				temp.append(line[i])
+				
+
 
 				print(temp)
 				res.append(temp)
@@ -51,7 +67,7 @@ def main():
 			
 			count += 1
 	
-	data = pd.DataFrame(res)
+	data = pd.DataFrame(res, columns=['Full Name', 'ASURITE', 'GitHub', 'EmailID', 'Preferences', 'Avoidance', 'TimeZone', 'TimePreference', 'GithubKnowledge', 'ScrumKnowledge', 'Comments'])
 	data.to_csv("Output.csv")
 
 if __name__ == "__main__":
