8dcca0528a892afa0367c69f43937b3b616ffecc
tpalkar92@gmail.com <tpalkar92@gmail.com>
2019-03-18 01:47:30 -0700

    Updated coursepage with functionalities to send and receive data

--- application.py
+++ application.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template, url_for,json, request
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired 
+
 app = Flask(__name__)
+app.config['SECRET_KEY']= 'Secret'
+
+# class TokenForm(Form):
+# 	tokenid = StringField('tokenid',validators =[InputRequired()])
+
+# @app.route('/', methods =['GET', 'POST'])
+# def form(): 
+# 	form =TokenForm()
+# 	if form.validate_on_submit():
+# 		return 'Form Successfully submitted'
+# 	return render_template('form.html', form=form)
 
 with open('courseslist.json') as f:
     courses = json.load(f)
@@ -8,6 +23,11 @@ with open('courseslist.json') as f:
 @app.route("/home")
 def home():
     return render_template('home.html',title ="Home",courses= courses)
+
+@app.route("/newLogin")
+def newLogin():
+    return render_template('newLogin.html',title ="Login")
+
 
 @app.route("/about")
 def about():
--- static/jquery.js
+++ static/jquery.js
@@ -3,4 +3,45 @@ $(document).ready(function(){
 		$("#content1").hide()
 		$("#content2").show();
 	})
-})
+	$("#home").click(function(){
+		$("#content2").hide()
+		$("#content1").show();
+	})
+
+	var acc = document.getElementsByClassName("accordion");
+
+	for (var i = 0; i < acc.length; i++) {
+	  	acc[i].addEventListener("click", function() {
+	    this.classList.toggle("active");
+	    var panel = this.nextElementSibling;
+	    if (panel.style.maxHeight){
+	      panel.style.maxHeight = null;
+	    } else {
+	      panel.style.maxHeight = panel.scrollHeight + "px";
+	    } 
+	  });
+	}
+
+});
+
+function uploadFile(){
+	var x = document.getElementsById("file");
+	x.disabled = true;
+}
+$(document).ready(
+    function(){
+        $('input:file').change(
+            function(){
+                if ($(this).val()) {
+                    $('input:submit').attr('disabled',false); 
+                    $('#remove-file').attr('disabled',false);
+                } 
+            }
+            );
+    });
+$('#remove-file').on('click',function(e){
+	var $el = $('#file');
+	$el.wrap('<form>').closest('form').get(0).reset();
+	$el.unwrap();
+});
+	
