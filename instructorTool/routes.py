ec233b3e28227d69367814c254bd6dd36f61154b
Zain Siddiqui <znsiddiqui1@gmail.com>
2019-04-01 20:30:12 -0700

    Task 145
    - Refactor code
    - Created new object class for fetch

--- instructorTool/routes.py
+++ instructorTool/routes.py
@@ -17,6 +17,7 @@ import pandas as pd
 import traceback
 import sys
 from instructorTool.Group_Scripts.group_online import OnlineGroup
+from instructorTool.Group_Scripts.fetch import FetchInfo
 from flask import Flask, session
 
 course = '15760'
@@ -123,84 +124,87 @@ def account():
 @app.route("/document")
 @login_required
 def fetch_document(doc_id, pref, avoid, group_size):
-    doc_id = doc_id.split("/")[5]
-    pref = int(pref)
-    avoid = int(avoid)
-    group_size = int(group_size)
-    print(doc_id)
-    access_token = session['access_token']
-    r=requests.get("https://www.googleapis.com/drive/v3/files/"+doc_id+"/export?mimeType=text/csv", headers={"Authorization":access_token})
+    # doc_id = doc_id.split("/")[5]
+    # pref = int(pref)
+    # avoid = int(avoid)
+    # group_size = int(group_size)
+    # print(doc_id)
+    # access_token = session['access_token']
+    # r=requests.get("https://www.googleapis.com/drive/v3/files/"+doc_id+"/export?mimeType=text/csv", headers={"Authorization":access_token})
    
-    destname = 'dummy.csv'
-    with open(destname, 'w') as wf:
-        wf.write(r.text)
+    # destname = 'dummy.csv'
+    # with open(destname, 'w') as wf:
+    #     wf.write(r.text)
         
-    count = 0
-    with open(destname, 'r') as rf:
-        rawtext = rf.read().splitlines()
-        myreader = csv.reader(rawtext)
-        res = []
-        no_of_pref = pref
-        no_of_avoid = avoid
-        for row in myreader:
-            print("-----------------------------------------------------------------------")
-            count = count + 1
-            if(count % 2 != 0 and count > 1):
-                try:
-                    temp = []
-                    i = 1
-                    temp.append(row[i])
-                    i+=1
-                    temp.append(row[i])
-                    i+=1
-                    temp.append(row[i])
-                    i+=1
-                    temp.append(row[i])
-                    i += 1
-                    pref = []
-                    for k in range(i, no_of_pref+i):
-                        #print(line[k])
-                        if row[k] != '' and row[k] != ' ':
-                            #print(line[k])
-                            pref.append(row[k])
-                    temp.append(pref)
-                    i += no_of_pref
-                    avoid = []
-                    for k in range(i, no_of_avoid+i):
-                        if row[k] != '' and row[k] != ' ':
-                            avoid.append(row[k])
-                    temp.append(avoid)
-                    i += no_of_avoid
-                    temp.append(row[i])
-                    i += 1
-                    date_time = []
-                    for idx,word in enumerate(row[i].split(',')):
-                        if idx%2 == 0:
-                            first = word
-                        else:
-                            date_time.append(first + '-' + word)
+    # count = 0
+    # with open(destname, 'r') as rf:
+    #     rawtext = rf.read().splitlines()
+    #     myreader = csv.reader(rawtext)
+    #     res = []
+    #     no_of_pref = pref
+    #     no_of_avoid = avoid
+    #     for row in myreader:
+    #         print("-----------------------------------------------------------------------")
+    #         count = count + 1
+    #         if(count % 2 != 0 and count > 1):
+    #             try:
+    #                 temp = []
+    #                 i = 1
+    #                 temp.append(row[i])
+    #                 i+=1
+    #                 temp.append(row[i])
+    #                 i+=1
+    #                 temp.append(row[i])
+    #                 i+=1
+    #                 temp.append(row[i])
+    #                 i += 1
+    #                 pref = []
+    #                 for k in range(i, no_of_pref+i):
+    #                     #print(line[k])
+    #                     if row[k] != '' and row[k] != ' ':
+    #                         #print(line[k])
+    #                         pref.append(row[k])
+    #                 temp.append(pref)
+    #                 i += no_of_pref
+    #                 avoid = []
+    #                 for k in range(i, no_of_avoid+i):
+    #                     if row[k] != '' and row[k] != ' ':
+    #                         avoid.append(row[k])
+    #                 temp.append(avoid)
+    #                 i += no_of_avoid
+    #                 temp.append(row[i])
+    #                 i += 1
+    #                 date_time = []
+    #                 for idx,word in enumerate(row[i].split(',')):
+    #                     if idx%2 == 0:
+    #                         first = word
+    #                     else:
+    #                         date_time.append(first + '-' + word)
 
-                    temp.append(date_time)
-                    i += 1
-                    temp.append(row[i])
-                    i += 1
-                    temp.append(row[i])
-                    i += 1
-                    temp.append(row[i])
-                    res.append(temp)
-                    print(count, temp)
-                except:
-                    traceback.print_exc(file=sys.stdout)
+    #                 temp.append(date_time)
+    #                 i += 1
+    #                 temp.append(row[i])
+    #                 i += 1
+    #                 temp.append(row[i])
+    #                 i += 1
+    #                 temp.append(row[i])
+    #                 res.append(temp)
+    #                 print(count, temp)
+    #             except:
+    #                 traceback.print_exc(file=sys.stdout)
 
-        data = pd.DataFrame(res, columns=['Full Name', 'ASURITE', 'GitHub', 'EmailID', 'Preferences', 'Avoidance', 'TimeZone', 'TimePreference', 'GithubKnowledge', 'ScrumKnowledge', 'Comments'])
-        session['response'] = data.to_json(orient='split')
-        print(data)
-        print("--------------Done----------------")
-    print("calling G")
-    g = OnlineGroup(group_size, data)
-    res = g.assign_group()
-    print(res)
+    #     data = pd.DataFrame(res, columns=['Full Name', 'ASURITE', 'GitHub', 'EmailID', 'Preferences', 'Avoidance', 'TimeZone', 'TimePreference', 'GithubKnowledge', 'ScrumKnowledge', 'Comments'])
+    #     session['response'] = data.to_json(orient='split')
+    #     print(data)
+    #     print("--------------Done----------------")
+    # print("calling G")
+    # g = OnlineGroup(group_size, data)
+    # res = g.assign_group()
+    # print(res)
 
+    f = FetchInfo(doc_id, pref, avoid, group_size)
+    res = f.fetch_document()
+    session['response'] = res.to_json(orient='split')
     return session['response']
 
 @app.route("/oauthcallback")
