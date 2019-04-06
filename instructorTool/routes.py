6d9637acdd66f33835c64bab2a33ecf7ea0da8d2
Ganesh Kumar <svganesh93@gmail.com>
2019-04-06 16:50:56 -0700

    Redirected to login page after logging out and removed commented code

--- instructorTool/routes.py
+++ instructorTool/routes.py
@@ -143,7 +143,7 @@ def send():
 @app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('login'))
 
 @app.route("/account")
 @login_required
@@ -153,88 +153,10 @@ def account():
 @app.route("/document")
 @login_required
 def fetch_document(doc_id, pref, avoid, group_size):
-
-    # doc_id = doc_id.split("/")[5]
-    # pref = int(pref)
-    # avoid = int(avoid)
-    # group_size = int(group_size)
-    # print(doc_id)
-    # access_token = session['access_token']
-    # r=requests.get("https://www.googleapis.com/drive/v3/files/"+doc_id+"/export?mimeType=text/csv", headers={"Authorization":access_token})
-
-    # destname = 'dummy.csv'
-    # with open(destname, 'w') as wf:
-    #     wf.write(r.text)
-
-    # count = 0
-    # with open(destname, 'r') as rf:
-    #     rawtext = rf.read().splitlines()
-    #     myreader = csv.reader(rawtext)
-    #     res = []
-    #     no_of_pref = pref
-    #     no_of_avoid = avoid
-    #     for row in myreader:
-    #         print("-----------------------------------------------------------------------")
-    #         count = count + 1
-    #         if(count % 2 != 0 and count > 1):
-    #             try:
-    #                 temp = []
-    #                 i = 1
-    #                 temp.append(row[i])
-    #                 i+=1
-    #                 temp.append(row[i])
-    #                 i+=1
-    #                 temp.append(row[i])
-    #                 i+=1
-    #                 temp.append(row[i])
-    #                 i += 1
-    #                 pref = []
-    #                 for k in range(i, no_of_pref+i):
-    #                     #print(line[k])
-    #                     if row[k] != '' and row[k] != ' ':
-    #                         #print(line[k])
-    #                         pref.append(row[k])
-    #                 temp.append(pref)
-    #                 i += no_of_pref
-    #                 avoid = []
-    #                 for k in range(i, no_of_avoid+i):
-    #                     if row[k] != '' and row[k] != ' ':
-    #                         avoid.append(row[k])
-    #                 temp.append(avoid)
-    #                 i += no_of_avoid
-    #                 temp.append(row[i])
-    #                 i += 1
-    #                 date_time = []
-    #                 for idx,word in enumerate(row[i].split(',')):
-    #                     if idx%2 == 0:
-    #                         first = word
-    #                     else:
-    #                         date_time.append(first + '-' + word)
-    #                 temp.append(date_time)
-    #                 i += 1
-    #                 temp.append(row[i])
-    #                 i += 1
-    #                 temp.append(row[i])
-    #                 i += 1
-    #                 temp.append(row[i])
-    #                 res.append(temp)
-    #                 print(count, temp)
-    #             except:
-    #                 traceback.print_exc(file=sys.stdout)
-
-    #     data = pd.DataFrame(res, columns=['Full Name', 'ASURITE', 'GitHub', 'EmailID', 'Preferences', 'Avoidance', 'TimeZone', 'TimePreference', 'GithubKnowledge', 'ScrumKnowledge', 'Comments'])
-    #     session['response'] = data.to_json(orient='split')
-    #     print(data)
-    #     print("--------------Done----------------")
-    # print("calling G")
-    # g = OnlineGroup(group_size, data)
-    # res = g.assign_group()
-    # print(res)
-
     f = FetchInfo(doc_id, pref, avoid, group_size)
     res = f.fetch_document()
     session['response'] = res.to_json(orient='split')
-    return session['response']
+    return res.to_json(orient='split')
 
 @app.route("/oauthcallback")
 def callback():
