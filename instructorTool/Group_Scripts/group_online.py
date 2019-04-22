b89009d48b6acb0c6ba59e9f01f5079909d37c7b
Zain Siddiqui <znsiddiqui1@gmail.com>
2019-04-22 11:59:29 -0700

    Task 275
    Code Refactored.

--- instructorTool/Group_Scripts/group_online.py
+++ instructorTool/Group_Scripts/group_online.py
@@ -4,12 +4,16 @@ import numpy
 from flask import session
 from instructorTool.Canvas_Scripts.student_list import Student_List
 
+# This File utilizes the fetched responses from survey results
+# and create groups for online course students.
+
 class OnlineGroup:
+	# Defining all the data structures to be used.
+	# inp - team size from user, dataframe - a dictionary which holds parsed result.
 	def __init__(self, team_size, dataframe):
 		self.dataframe = dataframe
 		self.no_of_stu = team_size
 		self.stu_list = [x for x in dataframe['ASURITE']]
-		print(self.stu_list)
 		self.stu_time_zone = {x:y for x,y in zip(dataframe['ASURITE'],dataframe['TimePreference'])}
 		self.stu_pref = {x:y for x,y in zip(dataframe['ASURITE'],dataframe['Preferences'])}
 		self.stu_avoid = {x:y for x,y in zip(dataframe['ASURITE'],dataframe['Avoidance'])}
@@ -17,29 +21,22 @@ class OnlineGroup:
 		self.all_stu = {}
 		
 		
-
+	# function to find similarity between time-list
 	def common_member(self, a,b):
 		return len(a & b)
 	
+	# function to read session values and validate students who did not took the survey. 
 	def read_data(self):
-		# This will read data from organized csv file for input
-		# with open("Output.csv") as fp:
-		# 	reader = csv.reader(fp)
-		# 	count = 0
-		# 	for line in reader:
-		# 		#print(line)
-		# 		if count > 0:
-		# 			self.stu_list.append(line[2])
-		# 			self.stu_pref[line[2]] = [x.replace("'",'') for x in line[5][1:-1].split(', ')]
-		# 			self.stu_avoid[line[2]] = [x.replace("'",'') for x in line[6][1:-1].split(', ')]
-		# 			self.stu_utc[line[2]] = line[7]
-		# 			self.stu_time_zone[line[2]] = line[8][1:-1].split(', ')
-		# 		count += 1
+		
 		token = session['canvas_token']
 		course_id = session['course_id']
+
+		# Getting all students enrolled in course
 		st_obj = Student_List(token)
 		self.all_stu = st_obj.get_student_list(course_id)
 		print('All students: ', self.all_stu)
+		
+		# Validating student list and adding student entry not available in survey.
 		idx = len(self.stu_list)
 		for s in self.all_stu:
 			if s not in self.stu_list:
@@ -51,18 +48,21 @@ class OnlineGroup:
 				self.dataframe.loc[idx] = [self.all_stu[s],s,'',s+'@asu.edu',[],[],[],[],0,0,'']
 				idx += 1 
 
+		
 		print("Finalized stu list:", self.stu_list)
 		for i in range(len(self.dataframe)):
 			print(self.dataframe.loc[i])
 
 
-		
+		# Creating unique set-list for student to avoid redundancy.
 		self.stu_available = set(self.stu_list)
 
+	# Function to create cost matrix based on student priorities.
 	def generate_cost_matrix(self):
 		
 		self.read_data()
 		
+		# Initializing the matrix variable and default team map.
 		max_factor = max(len(self.stu_time_zone[d]) for d in self.stu_time_zone)
 		n = len(self.stu_available)
 		self.team_map = {x:[] for x in range(1, (n // self.no_of_stu) + 1)}
@@ -71,11 +71,12 @@ class OnlineGroup:
 		#-------Create Distance Matrix-------
 		for i in range(n):
 			
+			# Condition for adding preference
 			temp1 = self.stu_pref[self.stu_list[i]]
 			if len(temp1) > 0 and temp1[0] != ' ':
 				a = 0
 				while a < len(temp1):
-					#print(temp1)
+					
 					try:
 
 						idx = self.stu_list.index(temp1[a])
@@ -87,6 +88,7 @@ class OnlineGroup:
 						print(temp1[a], " Not in the list")
 					a += 1
 
+			# Condition for adding avoidance
 			temp2 = self.stu_avoid[self.stu_list[i]]
 			if len(temp2) > 0 and temp2[0] != ' ':
 
@@ -103,12 +105,15 @@ class OnlineGroup:
 					except:
 						print(temp2[b], " Not in the list")
 					b += 1
+
+			# Condition for adding UTC weight
 			for j in range(i+1, n):
 				if i != j:
 					if self.stu_utc[self.stu_list[i]] == self.stu_utc[self.stu_list[j]]:
 						stu_dist[i][j] -= n
 						stu_dist[j][i] -= n
 
+			# Condition for adding Time preference weight
 			for j in range(i+1,n):
 				lst1 = self.stu_time_zone[self.stu_list[i]]
 				lst2 = self.stu_time_zone[self.stu_list[j]]
@@ -120,21 +125,24 @@ class OnlineGroup:
 					stu_dist[i][j] -= factor
 					stu_dist[j][i] -= factor
 
+		# Finalized matrix
 		return stu_dist
 
+	# Function to generate groups based on cost matrix
 	def generate_group(self):
 
 		stu_dist = self.generate_cost_matrix()
 
-			#----------formulating team---------
+		#----------formulating team---------
 		team_list = []
 		for i,s in enumerate(stu_dist):
 			if self.stu_list[i] in self.stu_available:
 				temp_team = []
-				#count_member = 1
 				temp_team.append(self.stu_list[i])
 				self.stu_available.remove(self.stu_list[i])
 				c = 1
+				# Check for equal opportunity in building team. c <= 3 is on the basis of maximum preference allowed by
+				# our tool. 
 				while self.no_of_stu > len(temp_team) and c <= 3: #self.no_of_stu:
 					idx_min = numpy.argmin(s)
 					if self.stu_list[idx_min] in self.stu_available:
@@ -144,7 +152,7 @@ class OnlineGroup:
 					s[idx_min] = 0
 					c += 1
 
-					#count_member += 1
+				
 
 				team_list.append(temp_team)
 
@@ -157,9 +165,8 @@ class OnlineGroup:
 		#remaining_students = list(stu_available)
 		if len(remaining_students) > 0:
 			
-			
 			print("remaining", remaining_students)
-
+			# Assigning teams to remaining students.
 			for team in team_list:
 				idx = 0
 				while len(team) < self.no_of_stu and idx < len(remaining_students):
@@ -181,12 +188,12 @@ class OnlineGroup:
 
 		return team_list
 
-
+	# Function to map the team list to group numbers.
 	def assign_group(self):
 
 		team_list = self.generate_group()
 
-			#-------------assigning team------------------
+		#-------------assigning team------------------
 		i = 0
 		for k in self.team_map:
 			self.team_map[k] = team_list[i]
@@ -194,7 +201,7 @@ class OnlineGroup:
 
 		print(self.team_map)
 
-		#-----------Allot Team numbers for excel----------
+		#-----------Allot Team numbers for dataframe----------
 		group_assign = [0]*len(self.stu_list)
 		print(self.stu_list)
 		for t in self.team_map:
@@ -203,19 +210,17 @@ class OnlineGroup:
 				group_assign[idx] = t
 		print(group_assign)
 		
-		#read_pd = pd.read_csv('Output.csv')
+		
 		read_pd = self.dataframe
+		# Add new group name
 		read_pd['Group Name'] = group_assign
 		print(read_pd)
 		return read_pd
-		#read_pd.to_csv('FinalOutput.csv')
+		
 
 
 
-
-
-
-# This method is purely for testing purpose
+# This method is purely for testing purpose 
 # def main():
 # 	#Read the group size
 # 	print("Enter number of students in a Team:", end="")
