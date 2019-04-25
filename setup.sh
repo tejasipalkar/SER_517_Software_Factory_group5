1fab1dc045784194fd875cfbbd53ebc96ea95690
Yathartha Goel <ygoel@asu.edu>
2019-04-25 14:38:44 -0700

    Task #309 Added a setup file for linux/mac and cleaned the folders

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pip install flask
+pip install flask_login
+pip install flask_sqlalchemy
+pip install flask_wtf
+pip install canvasapi
+pip install python-taiga
+pip install pandas
+pip install jwt
